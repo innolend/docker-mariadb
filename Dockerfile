daf46bcbe2ec296dfb7612e23c8d5720f8c3ad86
Paulius Kaminskas <paulius.kaminskas@nfq.lt>
2017-06-29 10:10:33 +0300

    Added entrypoint dumps, updated few mysql configuration values

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM alpine:latest
 WORKDIR /app
 VOLUME /app
 COPY startup.sh /startup.sh
-RUN chmod 755 /startup.sh
-RUN chmod +x /startup.sh
+RUN chmod 755 /startup.sh && chmod +x /startup.sh
 
 RUN apk add --update mysql mysql-client && rm -f /var/cache/apk/*
+RUN mkdir /docker-entrypoint-initdb.d
 COPY my.cnf /etc/mysql/my.cnf
 
 EXPOSE 3306
--- startup.sh
+++ startup.sh
@@ -7,11 +7,12 @@ else
 
   mysql_install_db --user=root > /dev/null
 
-  if [ "$DB_ROOT_PASSWORD" = "" ]; then
+  if [ -z "$DB_ROOT_PASSWORD" ]; then
     DB_ROOT_PASSWORD=111111
     echo "[i] MySQL root Password: $DB_ROOT_PASSWORD"
   fi
 
+  INITIALIZE_WITH_DUMP=${INITIALIZE_WITH_DUMP:-""}
   DB_DATABASE=${DB_DATABASE:-""}
   DB_USERNAME=${DB_USERNAME:-""}
   DB_PASSWORD=${DB_PASSWORD:-""}
@@ -33,19 +34,31 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 EOF
 
-  if [ "$DB_DATABASE" != "" ]; then
+  if [ ! -z "$DB_DATABASE" ]; then
     echo "[i] Creating database: $DB_DATABASE"
     echo "CREATE DATABASE IF NOT EXISTS \`$DB_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-    if [ "$DB_USERNAME" != "" ]; then
+    if [ ! -z "$DB_USERNAME" ]; then
       echo "[i] Creating user: $DB_USERNAME with password $DB_PASSWORD"
-      echo "GRANT ALL ON \`$DB_DATABASE\`.* to '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> $tfile      
+      echo "GRANT ALL ON \`$DB_DATABASE\`.* to '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> $tfile
+    fi
+
+    if [ ! -z "$INITIALIZE_WITH_DUMP" ]; then
+      echo "USE $DB_DATABASE;" >> $tfile
+      echo "[i] Importing dump files"
+
+      for f in /docker-entrypoint-initdb.d/*; do
+        case "$f" in
+          *.sql)    echo "$0: running $f"; cat "$f" >> $tfile; echo ;;
+#          *.sql.gz) echo "$0: running $f"; gunzip -c "$f" >> $tfile ; echo ;;
+          *)        echo "$0: ignoring $f" ;;
+        esac
+      done
     fi
   fi
 
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
   rm -f $tfile
 fi
-
 
 exec /usr/bin/mysqld --user=root --console
